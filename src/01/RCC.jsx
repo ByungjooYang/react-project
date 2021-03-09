fe75075ad071a881f2d2cb4455759cabde97d5b8
ByungjooYang <byungjoo104@gmail.com>
2021-03-09 23:45:11 +0900

    prettier 적용

---
+++ src/01/RCC.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class RCC extends Component {
+    render() {
+        let text = '따옴표';
+        return <div name="name">{text}</div>;
+    }
+}
+
+export default RCC;
+
+/*
+RCC  : 기본 리액트 컴포넌트 코드 생성            (React Component)
+RCCP : 리액트 컴포넌트를 프로퍼티 타입과 함께 생성 (React Component Property type)
+RCFC : 리액트 컴포넌트를 생명주기 함수와 함께 생성 (React Component liFe Cycle)
+RPC  : 리액트 퓨어 컴포넌트 생성                (React Pure Component)
+RSC  : 함수형 컴포넌트 생성
+RSCP : 함수형 컴포넌트를 프로퍼티 타입과 함께 생성
+ */
