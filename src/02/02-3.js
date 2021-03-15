dd8fb6c2b04193484833d498f6aed3b37a583bb0
ByungjooYang <byungjoo104@gmail.com>
2021-03-15 22:05:45 +0900

    Javascript ES6 Spread Operator (전개 연산자)

--- src/02/02-3.js
+++ src/02/02-3.js
@@ -1,3 +1,4 @@
+'use strict';
 //전개 연산자
 let array1 = ['one', 'two'];
 let array2 = ['three', 'four'];
@@ -13,4 +14,19 @@ function func() {
     let args = Array.prototype.slice.call(this, arguments); // 특수 변수(arguments)를 사용해 함수 내 인자 항목들을 배열로 변환. func(a,b,c) => args[0] = a, args[1] = b
     let first = args[0];
     let other = args.slice(1, args.length); // 인덱스 범위에 해당하는 배열 요소를 추출. args.length가 전체 배열의 길이이므로 인덱스 범위 1번부터 마지막 항목까지 추출한다.
+}
+
+let array3 = ['one', 'two'];
+let array4 = ['three', 'four'];
+const combined4 = [...array1, ...array2]; // 두 배열 항목을 전개 연산자로 합침.
+
+const [first1, second1, three1 = 'empty', ...others] = array3;  //first1, second1 은 array3의 각 위치에 있는 요소를 추출했음. three1은 기본값과 함께 배열 요소를 추출.
+                                                                // others는 앞에서 추출하고 남은 요소를 추출함.
+function func1(...args) {
+    let [first, ...others] = args; // 함수 인자 배열을 args 변수에 할당함.
+}
+
+function func2(first, ...others){
+    let firstInES6 = first;
+    let othersInES6 = others;
 }
