9c6459a13761048da487b257f4a439fd62e31c2c
ByungjooYang <byungjoo104@gmail.com>
2021-03-29 22:34:05 +0900

    Component

--- src/03/PropsComponent.jsx
+++ src/03/PropsComponent.jsx
@@ -1,1 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types'; //prop-types : 라이브러리. 특수 변수 사용해 자료형을 정의함.
+
+class PropsComponent extends React.Component {
+    render() {
+      return (
+        <div className="message-container">
+          {this.props.name}
+        </div>
+      )
+    }
+}
+
+//자료형 선언 예제
+PropsComponent.propTypes = {
+  name: PropTypes.string,
+};
+
+export default PropsComponent;
--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import './App.css';
 import React, { Component } from 'react';
-import TodaysPlan from './03/TodaysPlan'; //파일 이름 확장자는 없어도 된다. 웹팩 코드 검색 확장자의 기능 덕분이다.
+import TodaysPlan from './03/TodaysPlan';
+import ChildComponent from './03/ChildComponent';
+import BooleanComponent from './03/BooleanComponent'; //파일 이름 확장자는 없어도 된다. 웹팩 코드 검색 확장자의 기능 덕분이다.
+
+/*class App extends React.Component {
+    render() {
+        const array = [1, 2, 3];
+        const obj   = {name: '제목', age: 30};
+        const node  = <h1>노드</h1>
+        const func = () => {console.log('메시지');};
+        return (
+            <div className="body">
+                <ChildComponent
+                    boolValue={true}
+                    numValue={1}
+                    arrayValue={array}
+                    objValue={obj}
+                    nodeValue={node}
+                    funcValue={func}
+                />
+            </div>
+        );
+    }
+}*/
+
+/*class MyComponent extends React.Component {
+    render() {
+        const name = this.props.name;
+        return <span>{name}</span>;
+    }
+}*/
 
 class App extends React.Component {
     render() {
         return (
-            <div className="body">
-
-            </div>
-        );
+          <div>
+              <div><b>지루할 때 : </b><BooleanComponent bored /></div>
+              <div><b>즐거울 때 : </b><BooleanComponent /></div>
+          </div>
+        )
     }
 }
-
 
 export default App;
 
