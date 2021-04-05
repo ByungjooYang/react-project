dd495958e942a291eca08be7c4c4d391ce1c65b3
ByungjooYang <byungjoo104@gmail.com>
2021-04-05 21:45:22 +0900

    count

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import ChildProperty from './03/ChildProperty';
 import StateExample from './03/StateExample';
 import ForceUpdateExample from './03/ForceUpdateExample';
 import LifecycleExample from './03/LifecycleExample';
+import Counter from './03/Counter';
+import NewCounter from './03/NewCounter';
 
 /*class App extends React.Component {
     render() {
@@ -99,7 +101,7 @@ class App extends Component {
 }
 */
 
-class App extends React.Component {
+/*class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { hasDestroy: false };
@@ -114,12 +116,37 @@ class App extends React.Component {
             <div>
               <div>
                 {this.state.hasDestroy ? null : <LifecycleExample/>}
-              </div>
-                {/*<ForceUpdateExample/>*/}
+              </div>*/
+                /*{<ForceUpdateExample/>}
             </div>
         );
     }
+}*/
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    
+    this.state = {count: 10};
+    this.resetCount = this.resetCount.bind(this);
+  }
+  resetCount(){
+    this.setState(({ count }) => ({
+      count: count + 10
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <div><Counter count={this.state.count} /></div>
+        <div><NewCounter count={this.state.count} /></div>
+        <button onClick={this.resetCount}>{this.state.count + 10}으로 초기화</button>
+      </div>
+    );
+  }
 }
+                
+
 
 export default App;
 
